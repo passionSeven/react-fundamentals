1c6fff8c6ffdf9c38d9633f70631e9f470f16ab5
passionSeven <angelvsdevil312@gmail.com>
2021-12-24 18:25:41 -0500

    05.1

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -4,45 +4,32 @@
 import * as React from 'react'
 import '../box-styles.css'
 
-// 💰 Use the className for the size and style (backgroundColor, fontStyle) for the color and the font style
-// 💰 each of the elements should also have the "box" className applied
+// Try to make a custom <Box /> component that renders a div, accepts all the props and merges the given style and className props with the shared values.
 
-// 🐨 add a className prop to each of these and apply the correct class names
-// 💰 Here are the available class names: box, box--large, box--medium, box--small
+// I should be able to use it like so:
 
-// 🐨 add a style prop to each of them as well so their background color
-// matches what the text says it should be as well as `fontStyle: 'italic'`
-const smallBox = (
-  <div
-    className="lightblue box box--small"
-    style={{fontStyle: 'italic', background: 'lightblue'}}
-  >
-    small lightblue box
-  </div>
-)
-const mediumBox = (
-  <div
-    className="pink box box--medium"
-    style={{fontStyle: 'italic', background: 'pink'}}
-  >
-    medium pink box
-  </div>
-)
-const largeBox = (
-  <div
-    className="orange box box--large"
-    style={{fontStyle: 'italic', background: 'orange'}}
-  >
-    large orange box
-  </div>
-)
+// The box className and fontStyle: 'italic' style should be applied in addition to the values that come from props.
+
+const Box = ({className, style, children}) => {
+  return (
+    <div className={'box ' + className} style={{fontStyle: 'italic', ...style}}>
+      {children}
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+      <Box className="box--small" style={{backgroundColor: 'lightblue'}}>
+        small lightblue box
+      </Box>
+      <Box className="box--medium" style={{backgroundColor: 'orange'}}>
+        medium orange box
+      </Box>
+      <Box className="box--large" style={{backgroundColor: 'goldenrod'}}>
+        large goldenrod box
+      </Box>
     </div>
   )
 }
